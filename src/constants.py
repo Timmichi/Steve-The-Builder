273acba27749017a88cf52be8f45173296e3e852
nikolapratte <nikolapratte@gmail.com>
2021-12-08 16:27:36 -0800

    Problem type descriptions

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,10 @@ from enum import auto, Enum
 
 
 class ProblemType(Enum):
+    # Completely flat world, agent defends itself from a Ghast using Cobblestone.
     flat = auto()
+    # Similar to flat world but involves a pyramid-like hill.
     hill = auto()
+    # Agent attempts to prevent itself and sheep from falling into water
+    # ponds scattered across the map.
     sheep_water = auto()
